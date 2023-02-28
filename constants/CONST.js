5bea4874cab6606f08c244911a0788f43fb952d5
Getabalew Tesfaye <Getudesta4@gmail.com>
2023-02-28 11:58:22 -0800

    introducing CONST.js to store all constant values and variables

---
+++ constants/CONST.js
@@ -0,0 +1,29 @@
+const NAV_MENUS = [
+    {
+        title: "መክሊት",
+        icon: "home",
+        navTo: "HomeScreen"
+    },
+    {
+        title: "እቃዎቼ",
+        icon: "fruit-grapes",
+        navTo: "HomeScreen"
+    },
+    {
+        title: "",
+        icon: "plus",
+        navTo: "HomeScreen"
+    },
+    {
+        title: "መልዕክት",
+        icon: "chatbubble-sharp",
+        navTo: "HomeScreen"
+    },
+    {
+        title: "አሰሚ",
+        icon: "notifications",
+        navTo: "HomeScreen"
+    }
+]
+
+export { NAV_MENUS }
