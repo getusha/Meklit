a79afef3c5f17c69ca5f39de8e77be9c2c06bef4
Getabalew Tesfaye <Getudesta4@gmail.com>
2023-03-02 05:49:55 -0800

    render HomeScreen after some time to prevent delays on tab navigator ui:

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,32 +1,37 @@
-import React, { useState } from "react";
-import { Dimensions, Image, ScrollView, StyleSheet, View, Text, Pressable, TouchableOpacity } from "react-native";
+import React, { useEffect, useState } from "react";
+import { ActivityIndicator, ScrollView, StyleSheet } from "react-native";
 import colors from "../constants/colors";
 
 import FilterSelector from "../components/FilterSelector";
 import ItemPost from "../components/ItemPost";
 import items from "../constants/items";
-import Icon from "react-native-vector-icons/Feather";
-import * as IIcon from "react-native-vector-icons/Ionicons";
-import meklit from "../assets/images/meklit.png";
-import * as MIcon from "react-native-vector-icons/MaterialCommunityIcons";
-import BottomNavigation from "../components/BottomNavigation/BottomNavigation";
+import SkeletonContent from "react-native-skeleton-content";
 
 export default function HomeScreen(): JSX.Element {
+  const [render, setRender] = useState<boolean>(false);
 
-  const [selectedIndex, setSelectedIndex] = useState(0);
+  useEffect(() => {
+    setTimeout(() => {
+      setRender(true)
+    }, 100)
+  }, [])
 
   return (
     <React.Fragment>
-
-      <ScrollView showsVerticalScrollIndicator style={styles.itemScrollContainer}>
+      {!render &&
+        <ActivityIndicator style={{
+          height: "100%",
+          width: "100%"
+        }} />
+      }
+      {render && <ScrollView
+        showsVerticalScrollIndicator
+        style={styles.itemScrollContainer}>
         <FilterSelector />
         {items.map((item, idx) => {
           return <ItemPost item={item.item} user={item.user} />
         })}
-      </ScrollView>
-
-      <BottomNavigation />
-
+      </ScrollView>}
     </React.Fragment>
   )
 }
