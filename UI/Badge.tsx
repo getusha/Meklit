154089e3317f06faabb96578d33111e43ff808f8
Getabalew Tesfaye <Getudesta4@gmail.com>
2023-03-07 11:34:11 -0800

    fix weird linter issue - temporary

--- UI/Badge.tsx
+++ UI/Badge.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import colors from "../constants/colors";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
