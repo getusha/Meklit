f1bf3ab9c6b478171cdeb5917f7981c07fb8bbfa
Getabalew Tesfaye <Getudesta4@gmail.com>
2023-03-07 11:43:42 -0800

    improve code readability

--- UI/Badge.tsx
+++ UI/Badge.tsx
@@ -3,16 +3,28 @@ import { StyleSheet, Text, View } from "react-native";
 import colors from "../constants/colors";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
-export default function Badge(props: { title: string, icon?: string, theme?: "red" | "yellow" | "green" | "notheme" }) {
+type BadgePropTypes = {
+    title: string,
+    icon?: string,
+    theme?: "red" | "yellow" | "green" | "notheme",
+}
+
+export default function Badge(props: BadgePropTypes) {
+    const isRedTheme = props.theme === "red";
+    const isGreenTheme = props.theme === "green";
+    const isYellowTheme = props.theme === "yellow";
+    const isDefaultTheme = props.theme === "notheme";
+
+
     return (
         <View style={
             [styles.badge,
             props.theme ?
                 // sorry fellow developers it was fun nesting this at the moment
-                (props.theme === "green" ? styles.badgeGreenTheme :
-                    (props.theme === "yellow" ? styles.badgeOrangeTheme :
-                        (props.theme === "red" ? styles.badgeRedTheme :
-                            (props.theme === "notheme" && {})))) : styles.badgeDefaultTheme]}
+            (isGreenTheme ? styles.badgeGreenTheme :
+            (isYellowTheme ? styles.badgeOrangeTheme :
+            (isRedTheme ? styles.badgeRedTheme :
+            (isDefaultTheme && {})))) : styles.badgeDefaultTheme]}
         >
             <Icon name={props.icon ?? "tag"} color={colors.primaryRed} />
             <Text style={styles.badgeText}>{props.title}</Text>
